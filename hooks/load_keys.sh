e9e753c8a081052e94322b1cc2d32dea326d6f8a
Elliot Saba <staticfloat@gmail.com>
2021-06-23 03:36:34 +0000

    Unlock at both environment and post-checkout
    `environment` so that we can load keys to perform the actual checkout.
    `post-checkout` for loading keys that are stored within the repo itself.

---
+++ hooks/load_keys.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -eou pipefail
+
+# Start up ssh-agent
+eval "$(ssh-agent -s)"
+
+# Load keyfiles off of disk
+IDX=0
+while [[ -v "BUILDKITE_PLUGIN_SSH_AGENT_KEYFILES_${IDX}" ]]; do
+    VARNAME="BUILDKITE_PLUGIN_SSH_AGENT_KEYFILES_${IDX}"
+    if [[ -f "${!VARNAME}" ]]; then
+        cat "${!VARNAME}" | ssh-add -
+    else
+        echo "Skipping ${!VARNAME} as it doesn't exist (yet)"
+    fi
+
+    IDX=$((${IDX} + 1))
+done
+
+# Decode environment variables, and pipe them in through `stdin`
+IDX=0
+while [[ -v "BUILDKITE_PLUGIN_SSH_AGENT_KEYVARS_${IDX}" ]]; do
+    # First, build buildkite plugin variable name
+    VARNAME="BUILDKITE_PLUGIN_SSH_AGENT_KEYVARS_${IDX}"
+    # Use that to get the actual keyfile variable name
+    VARNAME="${!VARNAME}"
+    # Then we dereference _again_ to get the actual keyfile contents
+    base64 -d <<< "${!VARNAME}" | ssh-add -
+
+    IDX=$((${IDX} + 1))
+done
+
+# List all loaded SSH keys
+ssh-add -l
